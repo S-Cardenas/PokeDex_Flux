aac3d56deea3d26c5e3263e2d04c9ba9d455a445
Patrick&Stefan <patrickklong@gmail.com>
2016-03-24 14:59:45 -0400

    Finish Phase 3

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,12 +1,32 @@
 var ReactDOM = require('react-dom'),
     React = require('react'),
-    PokemonsIndex = require('./components/pokemons/pokemons_index');
+    PokemonsIndex = require('./components/pokemons/pokemons_index'),
+    Router = require('react-router').Router,
+    Route = require('react-router').Route;
 
 
+var App = React.createClass({
+  render: function() {
+    return (
+      <div id="pokedex">
+        <div className="pokemon-index-pane">
+          <PokemonsIndex />
+        </div>
+      </div>
+    );
+  }
+});
 
 
 document.addEventListener("DOMContentLoaded", function() {
   ReactDOM.render(
-    <PokemonsIndex />, document.getElementById("root")
+    <Router>
+      <Route path="/" component={App} />
+    </Router>,
+
+
+
+
+    document.getElementById("root")
   );
 });
